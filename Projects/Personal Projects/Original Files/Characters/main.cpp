605a058a96e3e4d8642cb4b550e62c488a6b57bf
gitMaverickHub <133601671+gitMaverickHub@users.noreply.github.com>
2025-04-05 09:22:08 -0500

    Update main.cpp

--- Projects/Personal Projects/Original Files/Characters/main.cpp
+++ Projects/Personal Projects/Original Files/Characters/main.cpp
@@ -2,15 +2,19 @@
     Using a map does not work for this instance because there is not a constructor for a map with the second parameter, the value, being the type, array<vector<string>, 2>
     While adding new character, try to alphabetize the various lists eg Character example = Character("exampleCharacter", {"a","b","c"},{"a","d","e"},...)
 
-    Find a wat to make the terminal print indefinitely to the right rather than start a new line. Also adjust setwidth size. Find a way to set links. Set a way to exit filter early.
+    Find a way to make the terminal print indefinitely to the right rather than start a new line. Also adjust setwidth size. Find a way to set links. Set a way to exit filter early.
     Also make a way to input classes for places and subraces where if I put Hill Dwarf, Dwarf is autopopulated and if I put Kror Forest Faedron is autopopulated
     Consider things like this with magics as well and combat roles.
 
-    Find a way to print only desired date. For instance if I only want to know about a characters place and combat role, it prints only that
+    Find a way to print only desired data. For instance if I only want to know about a characters place and combat role, it prints only that
 
     Possibly make a template. Possibly make a recipes class similar to characters class and make an app from that. This would be good c++ project, app making, and web development ie c++, html, css, and javascript practice
 
     Possibly use multithreading if applicable
+
+    The output needs to be made neater
+
+    Turn Lionheart into a full website at some point with all the pages including character slides, location, locations slides, a home page, etc. Do not spend more time on this, but this a good template for how I would like a characters page structured. Make sure to add hyperlinks.
 */
 
 #include "characters.cpp"
